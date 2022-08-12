bd6f6699fc7b53b4991d3b4ca18b087717028015
林庭阳 <geeklin@126.com>
2022-08-12 12:03:45 +0800

    csharp 项目实践

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace csharp_project
+{
+    internal class Program
+    {
+        public static void Main(string[] args) {
+            Console.WriteLine("Hello World");
+        }
+    }
+}
